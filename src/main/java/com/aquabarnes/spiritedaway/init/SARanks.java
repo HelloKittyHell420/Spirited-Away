80d7da632dfa8f9220246ec289fd0b50fdcbcf25
AquaBarnes <aqua.mandrew@gmail.com>
2016-12-21 03:39:17 +0000

    no message

--- src/main/java/com/aquabarnes/spiritedaway/init/SARanks.java
+++ src/main/java/com/aquabarnes/spiritedaway/init/SARanks.java
@@ -11,7 +11,11 @@ public class SARanks
   {
     addDeveloperRank("c7b62bb0-8a7a-41f8-a9d9-812c89ee9c99");
     
-    addTesterRank("");
+    addTesterRank("c361a4ce-9961-4b6f-bb34-c04da5316680");
+    addTesterRank("2891d5d1-8fc7-4afd-a866-05c38078a5dd");
+    addTesterRank("2ae3627a-5eba-46f7-9c29-3119ae41c4ef");
+    
+    addTesterRank("b6310a91-919e-4f31-920b-42dbc15957a8");
     
   }
   
